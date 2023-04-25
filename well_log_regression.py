d03bb16d1c8958ec36cb662b0de96ac240a33266
Rafael Lima <rafalima7@yahoo.com.br>
2023-04-25 16:51:07 -0300

    Update well_log_regression.py

--- well_log_regression.py
+++ well_log_regression.py
@@ -5,3 +5,19 @@ Created on Tue Apr 25 16:41:09 2023
 @author: Rafael Lima
 """
 
+#%% Load Packages
+
+import lasio
+
+#%% Loading DataSet
+
+# alaska1 = lasio.read('well_logs/dados_alaska/dado1.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska2 = lasio.read('well_logs/dados_alaska/dado2.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska3 = lasio.read('well_logs/dados_alaska/dado3.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska4 = lasio.read('well_logs/dados_alaska/dado4.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska5 = lasio.read('well_logs/dados_alaska/dado5.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska6 = lasio.read('well_logs/dados_alaska/dado6.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska7 = lasio.read('well_logs/dados_alaska/dado7.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+# alaska8 = lasio.read('well_logs/dados_alaska/dado8.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+alaska9 = lasio.read('well_logs/dados_alaska/dado9.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+
