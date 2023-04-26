08eeae6a9d36488949701f305bcf9293066c35f2
Rafael Lima <rafalima7@yahoo.com.br>
2023-04-26 13:34:59 -0300

    Update well_log_regression.py

--- well_log_regression.py
+++ well_log_regression.py
@@ -8,16 +8,20 @@ Created on Tue Apr 25 16:41:09 2023
 #%% Load Packages
 
 import lasio
-
+import seaborn
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
+    
 #%% Loading DataSet
 
-# alaska1 = lasio.read('well_logs/dados_alaska/dado1.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+#Alaska dataSet
 alaska2 = lasio.read('well_logs/dados_alaska/dado2.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
 alaska3 = lasio.read('well_logs/dados_alaska/dado3.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
 alaska4 = lasio.read('well_logs/dados_alaska/dado4.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
 alaska5 = lasio.read('well_logs/dados_alaska/dado5.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
-alaska6 = lasio.read('well_logs/dados_alaska/dado6.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
-alaska7 = lasio.read('well_logs/dados_alaska/dado7.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
-# alaska8 = lasio.read('well_logs/dados_alaska/dado8.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
-alaska9 = lasio.read('well_logs/dados_alaska/dado9.las').df().loc[:,['RHOB','NPHI', 'DT', 'GR', 'ILD']].dropna(subset=['RHOB','NPHI', 'DT', 'GR', 'ILD'])
+
+X_alaska2 = alaska2.loc[:,['NPHI', 'DT', 'GR', 'ILD']]
+lof = LocalOutlierFactor()
+y_alaska2_lof = lof.fit_predict(X_alaska2)
+seaborn.pairplot(alaska2, hue=y_alaska2_lof)
 
