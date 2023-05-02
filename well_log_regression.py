6a692903d9a97f9a9d89f73a42f206dcf526f337
Rafael Lima <rafalima7@yahoo.com.br>
2023-05-02 14:00:18 -0300

    Update well_log_regression.py

--- well_log_regression.py
+++ well_log_regression.py
@@ -57,6 +57,84 @@ ax.axis('tight')
 ax.set_title('Matriz de Correlação sem pre-processamento - Alaska5', fontsize=14) # Adicionando um título à imagem
 fig.set_dpi(300)
 ax.table(cellText=table_data, colLabels=None, cellLoc='center', loc='center')
+#%% Aplicar Local outlier factor
+
+
+from sklearn.neighbors import LocalOutlierFactor
+
+lof = LocalOutlierFactor(n_neighbors=20)
+
+#### Alaska2
+alaska2_lof = lof.fit_predict(alaska2)
+alaska2['LOF'] = alaska2_lof
+seaborn.pairplot(alaska2, hue='LOF', palette='Dark2').fig.suptitle("Alaska 2 - LOF", y=1.05, fontsize=20)
+
+plt.figure(dpi=300, figsize=[20,20])
+plt.subplot(151)
+plt.scatter(alaska2['RHOB'], alaska2.index, c=alaska2_lof)
+plt.subplot(152)
+plt.scatter(alaska2['NPHI'], alaska2.index, c=alaska2_lof)
+plt.subplot(153)
+plt.scatter(alaska2['DT'], alaska2.index, c=alaska2_lof)
+plt.subplot(154)
+plt.scatter(alaska2['GR'], alaska2.index, c=alaska2_lof)
+plt.subplot(155)
+plt.scatter(alaska2['ILD'], alaska2.index, c=alaska2_lof)
+plt.xlim(0,40)
+
+alaska2 = alaska2.drop(alaska2[alaska2['LOF']==-1].index)
+alaska2 = alaska2.drop(['LOF'], axis = 1)
+alaska2.corr()
+seaborn.pairplot(alaska2, palette='Dark2')
+
+#### Alaska3
+lof = LocalOutlierFactor(n_neighbors=40)
+
+alaska3_lof = lof.fit_predict(alaska3)
+alaska3['LOF'] = alaska3_lof
+seaborn.pairplot(alaska3, hue='LOF', palette='Dark2').fig.suptitle("Alaska 3 - LOF", y=1.05, fontsize=20)
+
+plt.figure(dpi=300, figsize=[20,20])
+plt.subplot(151)
+plt.scatter(alaska3['RHOB'], alaska3.index, c=alaska3_lof)
+plt.subplot(152)
+plt.scatter(alaska3['NPHI'], alaska3.index, c=alaska3_lof)
+plt.subplot(153)
+plt.scatter(alaska3['DT'], alaska3.index, c=alaska3_lof)
+plt.subplot(154)
+plt.scatter(alaska3['GR'], alaska3.index, c=alaska3_lof)
+plt.subplot(155)
+plt.scatter(alaska3['ILD'], alaska3.index, c=alaska3_lof)
+plt.xlim(0,40)
+
+alaska3 = alaska3.drop(alaska3[alaska3['LOF']==-1].index)
+alaska3 = alaska3.drop(['LOF'], axis = 1)
+alaska3.corr()
+seaborn.pairplot(alaska3, palette='Dark2')
+
+#### Alaska5
+alaska5_cov = cov.fit_predict(alaska5.iloc[:,1:])
+alaska5['LOF'] = alaska5_cov
+seaborn.pairplot(alaska5, hue='LOF', palette='Dark2').fig.suptitle("Alaska 2 - Covariance", y=1.05, fontsize=20)
+
+plt.figure(dpi=300, figsize=[20,20])
+plt.subplot(151)
+plt.scatter(alaska5['RHOB'], alaska5.index, c=alaska5_cov)
+plt.subplot(152)
+plt.scatter(alaska5['NPHI'], alaska5.index, c=alaska5_cov)
+plt.subplot(153)
+plt.scatter(alaska5['DT'], alaska5.index, c=alaska5_cov)
+plt.subplot(154)
+plt.scatter(alaska5['GR'], alaska5.index, c=alaska5_cov)
+plt.subplot(155)
+plt.scatter(alaska5['ILD'], alaska5.index, c=alaska5_cov)
+plt.xlim(0,40)
+
+alaska5 = alaska5.drop(alaska5[alaska5['LOF']==-1].index)
+alaska5 = alaska5.drop(['LOF'], axis = 1)
+alaska5.corr()
+seaborn.pairplot(alaska5, palette='Dark2')
+
 #%% Aplicar o covariance
 
 from sklearn.covariance import EllipticEnvelope
@@ -64,7 +142,7 @@ from sklearn.covariance import EllipticEnvelope
 cov = EllipticEnvelope()
 
 #### Alaska2
-alaska2_cov = cov.fit_predict(alaska2.iloc[:,:3])
+alaska2_cov = cov.fit_predict(alaska2.iloc[:,0:3])
 alaska2['LOF'] = alaska2_cov
 seaborn.pairplot(alaska2, hue='LOF', palette='Dark2').fig.suptitle("Alaska 2 - Covariance", y=1.05, fontsize=20)
 
@@ -110,7 +188,7 @@ alaska3.corr()
 seaborn.pairplot(alaska3, palette='Dark2')
 
 #### Alaska5
-alaska5_cov = cov.fit_predict(alaska5)
+alaska5_cov = cov.fit_predict(alaska5.iloc[:,1:])
 alaska5['LOF'] = alaska5_cov
 seaborn.pairplot(alaska5, hue='LOF', palette='Dark2').fig.suptitle("Alaska 2 - Covariance", y=1.05, fontsize=20)
 
